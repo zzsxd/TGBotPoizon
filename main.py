890e38a0edab12e2e03810436ad00f35e9027c0d
zzsxd <nikitacapkov06@gmail.com>
2024-03-23 00:41:12 +0800

    change calculator and also add new buttons on keyboard

--- main.py
+++ main.py
@@ -16,30 +16,57 @@ def main():
     @bot.message_handler(commands=['start'])
     def start_message(message):
         buttons = Bot_inline_btns()
+        user_id = message.chat.id
         bot.send_message(message.chat.id, 'Привет, дорогой покупатель! Мы 95Shop предоставляем услугу заказов товаров '
                                           'с китайских маркетплейсов 🧯\n'
                                           '• Низкие комиссии\n'
                                           '• Частые скидки\n'
                                           '• Доставка 5-10 дней\n', reply_markup=buttons.start_btns())
+        temp_user_data.temp_data(user_id)[user_id][0] = 4
 
     @bot.message_handler(content_types=['text'])
     def text(message):
         user_id = message.chat.id
         code = temp_user_data.temp_data(user_id)[user_id][0]
+        buttons = Bot_inline_btns()
         if code == 0:
             rub = int(message.text)
-            result = rub * 13.5 + rub / 100 * 20 + 400
+            result = rub * 13.5 + 400
             bot.send_message(message.chat.id, f'Цена товара: {result}₽')
+            temp_user_data.temp_data(user_id)[user_id][0] = 4
         elif code == 1:
             rub = int(message.text)
-            result = rub * 13.5 + rub / 100 * 20 + 300
+            result = rub * 13.5 + 300
             bot.send_message(message.chat.id, f'Цена товара: {result}₽')
+            temp_user_data.temp_data(user_id)[user_id][0] = 4
         elif code == 2:
             rub = int(message.text)
-            result = rub * 13.5 + rub / 100 * 20 + 200
+            result = rub * 13.5 + 200
             bot.send_message(message.chat.id, f'Цена товара: {result}₽')
+            temp_user_data.temp_data(user_id)[user_id][0] = 4
         elif code == 3:
             bot.send_message(message.chat.id, 'Поздравляем! Вы получаете скидку 200₽ на любой заказ!')
+            temp_user_data.temp_data(user_id)[user_id][0] = 4
+        elif code == 4:
+            if message.text == 'Заказать с Poizon':
+                bot.send_message(message.chat.id, 'Для заказа товара с пойзон напишите модератору @virtosvskiwork')
+            elif message.text == 'Рассчитать стоимость':
+                bot.send_message(message.chat.id, 'Выберите тип товара', reply_markup=buttons.cost_btns())
+            elif message.text == 'Скидки':
+                bot.send_message(message.chat.id, 'Проверьте свои знания брендов и получите небольшой бонус!')
+                bot.send_message(message.chat.id, '1. В каком году был основан бренд Nike?',
+                                 reply_markup=buttons.test_questone_btns())
+            elif message.text == 'Заказать с TaoBao/1688':
+                bot.send_message(message.chat.id,
+                                 'Для заказа товара с таобао/1688 напишите модератору @virtosvskiwork\n'
+                                 'Внимательно проверяйте отзывы продавца на китайском марктеплейсе!\n'
+                                 'Сроки могут увеличиться на пару дней')
+            elif message.text == 'Помощь':
+                bot.send_message(message.chat.id, 'Если вы хотите задать вопрос -> @virtosvskiwork')
+            elif message.text == 'Отзывы':
+                bot.send_message(message.chat.id, 'Отзывы о заказах вы можете прочитать в нашем канале\n'
+                                                       'https://t.me/s95hop')
+
 
     @bot.callback_query_handler(func=lambda call: True)
     def callback(call):
@@ -47,11 +74,7 @@ def main():
         user_id = call.message.chat.id
         code = temp_user_data.temp_data(user_id)[user_id][0]
         buttons = Bot_inline_btns()
-        if call.data == 'poizon':
-            bot.send_message(call.message.chat.id, 'Для заказа товара с пойзон напишите модератору @virtosvskiwork')
-        elif call.data == 'cost':
-            bot.send_message(call.message.chat.id, 'Выберите тип товара', reply_markup=buttons.cost_btns())
-        elif call.data == 'boots':  # or call.data == 'shoes' or call.data == 'trousers' or call.data == 'accessory':
+        if call.data == 'boots':  # or call.data == 'shoes' or call.data == 'trousers' or call.data == 'accessory':
             bot.send_message(call.message.chat.id, 'Введите цену в Юанях (¥)')
             temp_user_data.temp_data(user_id)[user_id][0] = 0
         elif call.data == 'shoes' or call.data == 'trousers':
@@ -60,10 +83,6 @@ def main():
         elif call.data == 'accessory':
             bot.send_message(call.message.chat.id, 'Введите цену в Юанях (¥)')
             temp_user_data.temp_data(user_id)[user_id][0] = 2
-        elif call.data == 'discount':
-            bot.send_message(call.message.chat.id, 'Проверьте свои знания брендов и получите небольшой бонус!')
-            bot.send_message(call.message.chat.id, '1. В каком году был основан бренд Nike?',
-                             reply_markup=buttons.test_questone_btns())
         elif call.data == 'not_true1' or call.data == 'not_true2' or call.data == 'not_true3':
             bot.send_message(call.message.chat.id, 'Ответ не верный!')
         elif call.data == 'true1':
@@ -88,16 +107,6 @@ def main():
             bot.send_message(call.message.chat.id, 'Правильно! 4/5')
             bot.send_message(call.message.chat.id, '5. Ваши любимые кроссовки?')
             temp_user_data.temp_data(user_id)[user_id][0] = 3
-        elif call.data == 'taobao':
-            bot.send_message(call.message.chat.id,
-                             'Для заказа товара с таобао/1688 напишите модератору @virtosvskiwork\n'
-                             'Внимательно проверяйте отзывы продавца на китайском марктеплейсе!\n'
-                             'Сроки могут увеличиться на пару дней')
-        elif call.data == 'help':
-            bot.send_message(call.message.chat.id, 'Если вы хотите задать вопрос -> @virtosvskiwork')
-        elif call.data == 'review':
-            bot.send_message(call.message.chat.id, 'Отзывы о заказах вы можете прочитать в нашем канале\n'
-                                                   'https://t.me/s95hop')
     bot.polling(none_stop=True)
 
 
